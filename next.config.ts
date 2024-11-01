0fbd0c960853d879a2765239a84f88aa8a798999
crispylines <qrimeth@gmail.com>
2024-11-01 18:09:14 -0500

    fixes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    CLOUDFLARE_API_TOKEN: process.env.CLOUDFLARE_API_TOKEN || '',
-  },
-}
-
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    CLOUDFLARE_API_TOKEN: process.env.CLOUDFLARE_API_TOKEN || '',
+  }
+}
+
+module.exports = nextConfig
