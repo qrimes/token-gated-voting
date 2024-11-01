775d55ab063ebce39e3ac99c82615e692e7e1e47
crispylines <qrimeth@gmail.com>
2024-11-01 18:24:21 -0500

    im in too deep

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,24 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+
   env: {
+
     CLOUDFLARE_API_TOKEN: process.env.CLOUDFLARE_API_TOKEN || '',
+
+  },
+
+  output: 'standalone',
+
+  images: {
+
+    unoptimized: true
+
   }
+
 }
 
+
+
 module.exports = nextConfig
+
